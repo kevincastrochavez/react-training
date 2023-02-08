cc138eb90c044ef22bfc27f715eb2ce5a4da22e2
Kevin Castro <kevincastro@C02FD2RV-0db637.local>
2023-02-08 09:31:09 -0700

    created the color picker and the paint components to be able to get a random colro and some small colored boxes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Name from "./Name";
+import Paint from "./Paint";
 import PlayGround from "./PlayGround";
 
 function App() {
   return (
     <div className="App">
-      <PlayGround />
-      <Name />
+      {/* <PlayGround /> */}
+      {/* <Name /> */}
+      <Paint />
     </div>
   );
 }
