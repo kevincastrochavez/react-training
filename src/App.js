43945c92cbeabbda2d7f25e59c194eebead4126e
Kevin Castro <kevincastro@C02FD2RV-0db637.local>
2023-02-07 17:05:29 -0700

    installed the randomcolor package and added some more logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Name from "./Name";
 import PlayGround from "./PlayGround";
 
 function App() {
   return (
     <div className="App">
-    <PlayGround />
+      <PlayGround />
+      <Name />
     </div>
   );
 }
--- src/PlayGround.js
+++ src/PlayGround.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import randomColor from "randomcolor";
 
-function PlayGround() {
-    const [count, setCount] = useState(0);
-	return (
-		<div>
-			{count}
-			<button onClick={() => setCount((currentCount) => currentCount - 1)}>
-				-
-			</button>
-			<button onClick={() => setCount((currentCount) => currentCount + 1)}>
-				+
-			</button>
-		</div>
-	);
+export default function Playground() {
+  const [count, setCount] = useState(0);
+
+  const [color, setColor] = useState(null);
+  useEffect(() => {
+    setColor(randomColor());
+  }, [count]);
+
+  return (
+    <div style={{ borderTop: `10px solid ${color}` }}>
+      {count}
+      <button onClick={() => setCount((currentCount) => currentCount - 1)}>
+        -
+      </button>
+      <button onClick={() => setCount((currentCount) => currentCount + 1)}>
+        +
+      </button>
+    </div>
+  );
 }
-
-export default PlayGround
